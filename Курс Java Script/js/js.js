8ecce3b7f0515e5c2526a9719690866376341342
Rodeo007 <rdin-kvalev@mail.ru>
2015-11-20 17:26:21 +0300

    Домашка.Java 20 ноября

---
+++ Курс Java Script/js/js.js
@@ -0,0 +1,8 @@
+var x = 10, y = 20;
+var z;
+z = y;
+y = x;
+x = z;
+console.log (x,y);
+alert (x);
+alert (y);
---
+++ Курс Java Script/js/js1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rod on 20.11.15.
+ */
+
+var age=window.prompt("Сколько лет вашей бабушке?");
+age++;
+console.log (age);
+alert(age);
